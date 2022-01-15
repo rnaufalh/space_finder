f7cfd9213fc43a423c2451666d422150100a6e4d
rnaufalh <naufal.rayhan@gmail.com>
2022-01-15 15:43:56 +1000

    Add more choices to describe the perefered locations

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(SpaceFinder());
 
-enum SingingCharacter { open, closed }
+enum SpaceType { open, closed }
+enum QuietRoomChoices { yes, no }
 
 class SpaceFinder extends StatefulWidget {
   _SpaceFinderState createState() => _SpaceFinderState();
 }
 
 class _SpaceFinderState extends State<SpaceFinder> {
-  SingingCharacter? _character = SingingCharacter.open;
+  SpaceType? _spaceType = SpaceType.open;
+  QuietRoomChoices? _quietRoomChoices = QuietRoomChoices.no;
+
+  List<bool> _selections = List.generate(4, (_) => false);
 
   @override
   Widget build(BuildContext context) {
@@ -23,18 +28,54 @@ class _SpaceFinderState extends State<SpaceFinder> {
         body: Container(
           child: Column(
             children: <Widget> [
+              Center(
+                child: Text('Describe Your Preference'),
+              ),
+              Row(
+                children: <Widget> [
+                  Expanded(child: Text('Do you want a Quiet Room to study in?')),
+                  Expanded(
+                    child: ListTile(
+                      title: Text('Yes'),
+                      leading: Radio<QuietRoomChoices>(
+                        value: QuietRoomChoices.yes,
+                        groupValue: _quietRoomChoices,
+                        onChanged: (QuietRoomChoices? value) {
+                          setState(() {
+                            _quietRoomChoices = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListTile(
+                      title: Text('No'),
+                      leading: Radio<QuietRoomChoices>(
+                        value: QuietRoomChoices.no,
+                        groupValue: _quietRoomChoices,
+                        onChanged: (QuietRoomChoices? value) {
+                          setState(() {
+                            _quietRoomChoices = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
               Row(
                 children: <Widget> [
                   Expanded(child: Text('Open or Closed Space?')),
                   Expanded(
                     child: ListTile(
                       title: Text('Open'),
-                      leading: Radio<SingingCharacter>(
-                        value: SingingCharacter.open,
-                        groupValue: _character,
-                        onChanged: (SingingCharacter? value) {
+                      leading: Radio<SpaceType>(
+                        value: SpaceType.open,
+                        groupValue: _spaceType,
+                        onChanged: (SpaceType? value) {
                           setState(() {
-                            _character = value;
+                            _spaceType = value;
                           });
                         },
                       ),
@@ -43,18 +84,54 @@ class _SpaceFinderState extends State<SpaceFinder> {
                   Expanded(
                     child: ListTile(
                       title: Text('Closed'),
-                      leading: Radio<SingingCharacter>(
-                        value: SingingCharacter.closed,
-                        groupValue: _character,
-                        onChanged: (SingingCharacter? value) {
+                      leading: Radio<SpaceType>(
+                        value: SpaceType.closed,
+                        groupValue: _spaceType,
+                        onChanged: (SpaceType? value) {
                           setState(() {
-                            _character = value;
+                            _spaceType = value;
                           });
                         },
                       ),
                     ),
                   ),
                 ],
+              ),
+              Row(
+                children: <Widget> [
+                  Expanded(child: Text('How many are going?')),
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      initialValue: '1',
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 20.0,),
+              Text('Select your desired facilities:'),
+              SizedBox(height: 10.0,),
+              ToggleButtons(
+                  children: <Widget> [
+                    Icon(Icons.ac_unit),
+                    Icon(Icons.tv),
+                    Icon(Icons.microwave),
+                    Icon(Icons.local_drink),
+                  ],
+                  onPressed: (int index) {
+                    setState(() {
+                      _selections[index] = !_selections[index];
+                    });
+                  },
+                  isSelected: _selections
+              ),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      // TODO: Enable saving values to be used on next page
+                    });
+                  },
+                  child: Text('Show Results'),
               ),
             ],
           ),
