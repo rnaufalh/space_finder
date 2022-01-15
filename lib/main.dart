2ba175d4b3e45f6626f8804b0f9bb356b890deed
rnaufalh <naufal.rayhan@gmail.com>
2022-01-15 15:03:44 +1000

    Add choice on open or closed space

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,62 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(SpaceFinder());
 
-class SpaceFinder extends StatelessWidget {
+enum SingingCharacter { open, closed }
+
+class SpaceFinder extends StatefulWidget {
+  _SpaceFinderState createState() => _SpaceFinderState();
+}
+
+class _SpaceFinderState extends State<SpaceFinder> {
+  SingingCharacter? _character = SingingCharacter.open;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightGreenAccent,
+        backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text('Space Finder'),
           backgroundColor: Colors.lightBlue,
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget> [
+              Row(
+                children: <Widget> [
+                  Expanded(child: Text('Open or Closed Space?')),
+                  Expanded(
+                    child: ListTile(
+                      title: Text('Open'),
+                      leading: Radio<SingingCharacter>(
+                        value: SingingCharacter.open,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListTile(
+                      title: Text('Closed'),
+                      leading: Radio<SingingCharacter>(
+                        value: SingingCharacter.closed,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
